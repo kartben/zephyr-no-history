75bc8e1f474fd75c448fd980fe0d647e5d4ac93f
Benjamin Cabé <kartben@gmail.com>
2025-06-14 22:45:55 +0200

    fix coap option insertion

--- subsys/net/lib/coap/coap.c
+++ subsys/net/lib/coap/coap.c
@@ -134,9 +134,10 @@ static inline bool insert_be16(struct coap_packet *cpkt, uint16_t data, size_t o
 		return false;
 	}
 
-	memmove(&cpkt->data[offset + 2], &cpkt->data[offset], cpkt->offset - offset);
+       memmove(&cpkt->data[offset + 2], &cpkt->data[offset],
+               cpkt->offset - offset);
 
-	encode_be16(cpkt, cpkt->offset, data);
+       encode_be16(cpkt, offset, data);
 
 	return true;
 }
