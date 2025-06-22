48f66889978401a694a2ec7ac45e24fcc35ded06
Benjamin Cabé <kartben@gmail.com>
2025-06-22 09:44:38 +0200

    drivers: video: ov9655: init format struct

--- drivers/video/ov9655.c
+++ drivers/video/ov9655.c
@@ -326,7 +326,7 @@ static int ov9655_get_fmt(const struct device *dev, struct video_format *fmt)
 static int ov9655_init(const struct device *dev)
 {
 	const struct ov9655_config *config = dev->config;
-	struct video_format fmt;
+	struct video_format fmt = { 0 };
 	uint32_t pid;
 	int ret;
 
@@ -381,9 +381,11 @@ static int ov9655_init(const struct device *dev)
 	}
 
 	/* Set default camera format (QQVGA, YUYV) */
+	fmt.type = VIDEO_BUF_TYPE_OUTPUT;
 	fmt.pixelformat = VIDEO_PIX_FMT_YUYV;
 	fmt.width = 160;
 	fmt.height = 120;
+	fmt.pitch = fmt.width * video_bits_per_pixel(fmt.pixelformat) / BITS_PER_BYTE;
 
 	return ov9655_set_fmt(dev, &fmt);
 }
